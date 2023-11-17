998bd7ef2e9c706e2a16f35686207a1713d9fcb1
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2023-11-17 21:58:07 +0100

    Admin Accomodation Approval
    Added window for admin feature to approve or reject new accomodations

---
+++ BookingApp/app/src/main/java/com/example/bookingapp/adapters/AccomodationApprovalListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.bookingapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bookingapp.R;
+import com.example.bookingapp.model.Accommodation;
+
+import java.util.ArrayList;
+
+public class AccomodationApprovalListAdapter extends ArrayAdapter<Accommodation> {
+
+    private ArrayList<Accommodation> aAccommodations;
+
+    public AccomodationApprovalListAdapter(Context context, ArrayList<Accommodation> accommodations){
+        super(context, R.layout.list_approval_accomodation, accommodations);
+        aAccommodations = accommodations;
+    }
+
+    @Override
+    public int getCount() {
+        return aAccommodations.size();
+    }
+
+    @Nullable
+    @Override
+    public Accommodation getItem(int position) {
+        return aAccommodations.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Accommodation accommodation = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_accommodation,
+                    parent, false);
+        }
+        //LinearLayout accommodationCard = convertView.findViewById(R.id.accommodation_card_item);
+        ImageView imageView = convertView.findViewById(R.id.listImage);
+        TextView accommodationName = convertView.findViewById(R.id.listAccommodationName);
+        //TextView accommodationDescription = convertView.findViewById(R.id.listAccommodationDesc);
+
+        imageView.setImageResource(accommodation.getImage());
+        accommodationName.setText(accommodation.getName());
+        //accommodationDescription.setText(accommodation.getDescription());
+
+        return convertView;
+    }
+}
