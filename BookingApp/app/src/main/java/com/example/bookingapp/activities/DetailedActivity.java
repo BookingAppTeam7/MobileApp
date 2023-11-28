dd59a3b764cdaa762d72cc9a8195936ff99c9a3c
Luka Petković <lukapetkovicns21@gmail.com>
2023-11-28 12:12:09 +0100

    Set up initial Google Map

--- BookingApp/app/src/main/java/com/example/bookingapp/activities/DetailedActivity.java
+++ BookingApp/app/src/main/java/com/example/bookingapp/activities/DetailedActivity.java
@@ -1,15 +1,18 @@
 package com.example.bookingapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bookingapp.R;
 import com.example.bookingapp.databinding.ActivityDetailedBinding;
 
 public class DetailedActivity extends AppCompatActivity {
     ActivityDetailedBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +29,19 @@ public class DetailedActivity extends AppCompatActivity {
             binding.detailDescription.setText(description);
             binding.detailImage.setImageResource(image);
         }
+
+        binding.googleMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailedActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
