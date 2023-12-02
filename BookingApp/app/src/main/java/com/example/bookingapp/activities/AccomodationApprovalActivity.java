2677beedc87d67c7edec9c8b044a7d758d2cb80f
Luka Petković <lukapetkovicns21@gmail.com>
2023-12-02 17:56:57 +0100

    Fixed bug

--- BookingApp/app/src/main/java/com/example/bookingapp/activities/AccomodationApprovalActivity.java
+++ BookingApp/app/src/main/java/com/example/bookingapp/activities/AccomodationApprovalActivity.java
@@ -15,8 +15,13 @@ import com.example.bookingapp.adapters.AccomodationApprovalListAdapter;
 import com.example.bookingapp.databinding.ActivityAccomodationApprovalBinding;
 import com.example.bookingapp.databinding.ActivityHomeScreenBinding;
 import com.example.bookingapp.model.Accommodation;
+import com.example.bookingapp.model.Review;
+import com.example.bookingapp.model.ReviewType;
+import com.example.bookingapp.model.TimeSlot;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AccomodationApprovalActivity extends AppCompatActivity {
 
@@ -34,11 +39,36 @@ public class AccomodationApprovalActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         int[] imageList = {R.drawable.accommodation1, R.drawable.accommodation2, R.drawable.accommodation1, R.drawable.accommodation2};
         Long[] idList = {1L, 2L, 3L, 4L};
-        String[] nameList = {"Accommodation 1", "Accommodation 2", "Accommodation 3", "Accommodation 4"};
-        String[] descriptionList = {"Description 1", "Description 2", "Description 3", "Description 4"};
+        String[] nameList = {"Accommodation in Novi Sad", "Belgrade accommodation", "Prague accommodation", "Paris accommodation"};
+        String[] descriptionList = {"Beautiful accommodation settled in Novi Sad", "Beautiful accommodation settled in Belgrade",
+                "Beautiful accommodation settled in Prague", "Beautiful accommodation settled in Paris"};
+
+        double [] locationXList={45.26799224033295,44.78318632559004,50.102423832053915,48.870974300967234};
+        double [] locationYList={19.830824522193232, 20.49925984639849, 14.480125374774326, 2.478835370652442};
+        String [] locationStrList={"Some location in Novi Sad","Some location in Belgrade","Some location in Prague",
+                "Some location in Paris"};
+        double [] priceList={150.0,215.0,100.0,199.99};
+        List<Review> reviewsList = new ArrayList<>();
+
+
+        reviewsList.add(new Review(R.drawable.ic_user,"User1", ReviewType.OWNER, "Great experience!", 5));
+        reviewsList.add(new Review(R.drawable.ic_user,"User2", ReviewType.ACCOMMODATION, "Nice place to stay.", 4));
+        reviewsList.add(new Review(R.drawable.ic_user,"User3", ReviewType.OWNER, "Very helpful owner.", 5));
+        reviewsList.add(new Review(R.drawable.ic_user,"User4", ReviewType.ACCOMMODATION, "Clean and comfortable.", 4));
+
+        ArrayList<String> assets=new ArrayList<>();
+        assets.add("TV");
+        assets.add("WI-FI");
+        assets.add("Free parking");
+        assets.add("Air conditioner");
+
+        ArrayList<TimeSlot> availability=new ArrayList<>();
+        availability.add(new TimeSlot(1L, LocalDate.of(2023,12,3),LocalDate.of(2023,12,6)));
+        availability.add(new TimeSlot(1L, LocalDate.of(2023,12,9),LocalDate.of(2023,12,15)));
 
         for (int i = 0; i < imageList.length; i++) {
-            accommodation = new Accommodation(idList[i], nameList[i], descriptionList[i], imageList[i]);
+            accommodation = new Accommodation(idList[i], nameList[i], descriptionList[i], imageList[i],
+                    locationStrList[i],locationXList[i],locationYList[i],priceList[i],reviewsList,assets,availability);
             accommodationArrayList.add(accommodation);
         }
 
